b4e6c05dc04efe3c7e5edb8cc0be898d3f58192e
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2018-12-22 20:40:53 +0000

    bump release version

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,10 +3,13 @@
 Release History
 ---------------
 
-0.0.2 (unreleased)
+0.0.2 (2018-12-22)
 ++++++++++++++++++
+
+- Add long description for PyPi
+
 
 0.0.1 (2018-12-22)
 ++++++++++++++++++
 
-- Adds useful BatchHistory and PlotHistory callbacks
+- Add useful BatchHistory and PlotHistory callbacks
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 import os.path
 from setuptools import find_packages, setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # read the contents of your README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(
