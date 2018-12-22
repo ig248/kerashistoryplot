88d576bcbe15ec821933534bc97fa7c73a5d36ab
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2018-12-22 20:38:30 +0000

    add long description for PyPi

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
+import os.path
 from setuptools import find_packages, setup
 
 VERSION = '0.0.1'
 
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='kerashistoryplot',
     version=VERSION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=('tests', )),
     entry_points={'console_scripts': []},
     include_package_data=True,
